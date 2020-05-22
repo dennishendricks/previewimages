de6299aeafdeedc47c384e62e34e554761144937
Dennis Hagemeier <hagemeierd@Dennis-MacBook-Pro.local>
2020-05-22 16:19:36 +0200

    Modified cache dir

--- plugins/cache-directory/index.js
+++ plugins/cache-directory/index.js
@@ -1,12 +1,14 @@
 module.exports = {
-  async onPreBuild({ inputs, utils }) {
-    const cacheDir = inputs.cacheDir;
+  async onPreBuild({ inputs, constants, utils }) {
+    const cacheDir = constants.PUBLISH_DIR + inputs.cacheDir;
+    console.log(`Caching ${cacheDir}`);
     const files = await utils.cache.list();
     console.log("Cached files", files);
     await utils.cache.restore(cacheDir);
   },
-  async onPostBuild({ inputs, utils }) {
-    const cacheDir = inputs.cacheDir;
+  async onPostBuild({ inputs, constants, utils }) {
+    const cacheDir = constants.PUBLISH_DIR + inputs.cacheDir;
+    console.log(`Trying to get ${cacheDir}`);
     await utils.cache.save(cacheDir);
   },
 };
